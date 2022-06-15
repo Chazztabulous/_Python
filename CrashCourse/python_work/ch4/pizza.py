03e0585769249f9cc2e55d4c0d2cd8673d3ee24f
Nick <nstricklett@gmail.com>
2022-06-15 10:20:10 -0700

    python exersises

---
+++ CrashCourse/python_work/ch4/pizza.py
@@ -0,0 +1,9 @@
+# pizzas = ['hawaian', 'meat-lovers', 'cheese']
+# for pizza in pizzas:
+# 	print(f"I like {pizza} pizza")
+# print("I LOVE pizza!")
+
+pets = ['dog', 'cat', 'kowala']
+for pet in pets:
+	print(f" A {pet} would make a great pet! they are cuddly")
+print("All of these pets are adorable, and would make a great pet!")
---
+++ CrashCourse/python_work/ch4/squares.py
@@ -0,0 +1,29 @@
+squares = [value**2 for value in range(1, 11)]
+
+numbers = range(1,21)
+# for number in numbers:
+	# print(number)
+
+big_numbers = range(1, 1000001)
+# for num in big_numbers:
+# 	print(num)
+# print(min(big_numbers))
+# print(max(big_numbers))
+# print(sum(big_numbers))
+
+# odd_numbers = list(range(1, 21, 2))
+# for num in odd_numbers:	
+# 	print(num)
+
+multiples = list(range(3, 31, 3))
+# for three in multiples:
+# 	print(three)
+
+cubes = list(range(1, 11))	
+for cube in cubes:
+  print(cube**3)
+
+display = [value**3 for value in range(1,11)]
+print(display)
+
+
